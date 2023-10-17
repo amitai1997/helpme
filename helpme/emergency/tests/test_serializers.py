b3e1ce8df60c89b0ea07006cc13a3a0e9071d60c
amitai salmon <amitai1997@gmail.com>
2023-10-17 14:38:26 +0300

    create tests for profile serializers

---
+++ helpme/emergency/tests/test_serializers.py
@@ -0,0 +1,71 @@
+from rest_framework.test import APITestCase
+
+from helpme.emergency.api.serializers import ProfileSerializer
+from helpme.users.models import User
+
+
+class ProfileSerializerTests(APITestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(
+            name="testuser",
+            email="testuser@example.com",
+            password="testpassword",
+        )
+
+    def test_valid_serializer_data(self):
+        user = User.objects.get(email="testuser@example.com")
+        data = {
+            "user": user.pk,
+            "identification_number": "12345",
+            "gender": "Male",
+            "date_of_birth": "1990-01-01",
+            "city": "New York",
+            "preferred_area": "North",
+            "carrying_weapon": False,
+            "driving_license_tractor": True,
+        }
+        serializer = ProfileSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_invalid_identification_number(self):
+        data = {
+            "user": 1,
+            "identification_number": "abc",  # Invalid identification_number
+            "gender": "male",
+            "date_of_birth": "1990-01-01",
+            "city": "New York",
+            "preferred_area": "North",
+            "carrying_weapon": False,
+            "driving_license_tractor": True,
+        }
+        serializer = ProfileSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+    def test_invalid_gender(self):
+        data = {
+            "user": 1,
+            "identification_number": "12345",
+            "gender": "unknown",  # Invalid gender
+            "date_of_birth": "1990-01-01",
+            "city": "New York",
+            "preferred_area": "North",
+            "carrying_weapon": False,
+            "driving_license_tractor": True,
+        }
+        serializer = ProfileSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+    def test_invalid_city(self):
+        data = {
+            "user": 1,
+            "identification_number": "12345",
+            "gender": "male",
+            "date_of_birth": "1990-01-01",
+            "city": "N",  # Invalid city
+            "preferred_area": "North",
+            "carrying_weapon": False,
+            "driving_license_tractor": True,
+        }
+        serializer = ProfileSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
