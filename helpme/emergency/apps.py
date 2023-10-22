959ae350082a8d4acbb36fb55d0fe1121bac4d41
amitai salmon <amitai1997@gmail.com>
2023-10-22 19:53:20 +0300

    automatic creation of emergency types in model using post_migrate at apps.py

--- helpme/emergency/apps.py
+++ helpme/emergency/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def create_emergency_types(sender, **kwargs):
+    EmergencyType = sender.get_model("EmergencyType")
+    for choice in EmergencyType.EMERGENCY_CHOICES:
+        EmergencyType.objects.get_or_create(name=choice[0])
 
 
 class EmergencyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "helpme.emergency"
+
+    def ready(self):
+        post_migrate.connect(create_emergency_types, sender=self)
