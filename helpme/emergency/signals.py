d31ceddd9463305c293369931b3d5c187a2fbe7f
amitai salmon <amitai1997@gmail.com>
2023-10-26 18:13:04 +0300

    last working veersion

--- helpme/emergency/signals.py
+++ helpme/emergency/signals.py
@@ -1,12 +1,20 @@
 # signals.py in your app
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
 from helpme.emergency.models import EmergencyCall
 from helpme.emergency.tasks import schedule_emergency_tasks
 
+# @receiver(post_save, sender=EmergencyCall, dispatch_uid="handle_emergency_call_creation_signal")
+# @receiver(m2m_changed, sender=EmergencyCall.emergency_types.through)
+# def handle_emergency_call_creation(sender, instance, created, **kwargs):
+#     if created:
+#         schedule_emergency_tasks(instance.id)
 
-@receiver(post_save, sender=EmergencyCall, dispatch_uid="handle_emergency_call_creation_signal")
-def handle_emergency_call_creation(sender, instance, created, **kwargs):
-    if created:
+
+@receiver(m2m_changed, sender=EmergencyCall.emergency_types.through)
+def handle_emergency_types_change(sender, instance, action, reverse, model, pk_set, **kwargs):
+    if action == "post_add":
+        # This signal is triggered after new emergency types are added to the EmergencyCall
+        # You can access the updated instance with the populated emergency types here
         schedule_emergency_tasks(instance.id)
--- helpme/emergency/tasks.py
+++ helpme/emergency/tasks.py
@@ -1,12 +1,10 @@
 from time import sleep
 
 from celery import shared_task
-
-# from django.db.models import F, Q
 from django.contrib.gis.db.models.functions import Distance
 from django.core import serializers
 from django.core.mail import send_mail
-from django.db import transaction
+from django.db.models import Count
 
 from helpme.emergency.models import EmergencyCall, Notification, Volunteer
 
@@ -15,64 +13,94 @@ from helpme.emergency.models import EmergencyCall, Notification, Volunteer
 def schedule_emergency_tasks(emergency_call_id):
     try:
         emergency_call = EmergencyCall.objects.get(id=emergency_call_id)
-        matching_volunteers_json = find_matching_volunteers(emergency_call.to_json())
+        matching_volunteers_json = find_matching_volunteers(emergency_call)
         send_notifications(matching_volunteers_json, emergency_call.to_json())
     except EmergencyCall.DoesNotExist:
         # Handle the case where the EmergencyCall doesn't exist
-        pass
+        # Log the error and possibly notify administrators
+        import logging
+
+        logger = logging.getLogger(__name__)
+        logger.error(f"EmergencyCall with ID {emergency_call_id} does not exist.")
+        # You can also notify administrators or take other appropriate actions
+
+    except Exception as e:
+        # Handle other exceptions that may occur during the task execution
+        # Log the error and possibly notify administrators
+        import logging
+
+        logger = logging.getLogger(__name__)
+        logger.error(f"An error occurred while processing the task: {str(e)}")
+        # You can also notify administrators or take other appropriate actions
 
 
-def find_matching_volunteers(emergency_call_json):
-    emergency_call = EmergencyCall.from_json(emergency_call_json)
-    # Extract location and required skills from the emergency call (you need to implement this part)
+def find_matching_volunteers(emergency_call):
+    # Extract the required location and skills from the emergency call
     required_location = emergency_call.location
-    # required_skills = emergency_call.emergency_types.all()
+    required_skills = emergency_call.emergency_types.all()
 
+    # Calculate the distance between each volunteer's location and the required location
     matched_volunteers = (
         Volunteer.objects.annotate(distance=Distance("location", required_location))
-        .filter(distance__lte=100 * 1000)
-        .order_by("distance")
+        .filter(distance__lte=100 * 1000, skills__in=required_skills)  # 100 km in meters  # Filter by shared skills
+        .annotate(skill_count=Count("skills"))  # Count the number of shared skills
+        .filter(skill_count__gt=0)  # Filter volunteers with at least one shared skill
     )
 
-    # return matched_volunteers
+    # Serialize the matched volunteers to JSON
     matching_volunteers_json = serializers.serialize("json", matched_volunteers)
+
     return matching_volunteers_json
 
 
 @shared_task()
 def send_notifications(matching_volunteers_json, emergency_call_json):
+    # Deserialize JSON data to obtain matching volunteer objects
     matching_volunteers_data = serializers.deserialize("json", matching_volunteers_json, ignorenonexistent=True)
     matching_volunteers = [item.object for item in matching_volunteers_data]
+
+    # Extract user objects from matching volunteers
     matching_users = [volunteer.user for volunteer in matching_volunteers]
 
+    # Deserialize the emergency call JSON to get the emergency call object
     emergency_call = EmergencyCall.from_json(emergency_call_json)
-    # Implement the logic to create notifications and send them to matching volunteers
 
-    with transaction.atomic():
-        emergency_call.save()
-        notification = Notification.objects.create(
-            emergency_call=emergency_call,
-        )
+    # Create a notification for the emergency call
+    notification = Notification.objects.create(
+        emergency_call=emergency_call,
+    )
 
+    # Add matching users as receivers of the notification
     for user in matching_users:
         notification.receivers.add(user)
 
+    # Send notification emails to matching volunteers
     for volunteer in matching_volunteers:
         send_notification_email.delay(volunteer.user.email, emergency_call_json)
 
 
 @shared_task()
 def send_notification_email(email_address, emergency_call_json):
+    # Deserialize the emergency call JSON to get the emergency call object
     emergency_call = EmergencyCall.from_json(emergency_call_json)
+
     try:
-        sleep(15)  # Simulate expensive operation(s) that freeze Django
+        # Simulate an expensive operation that takes 7 seconds
+        sleep(7)
+
+        # Send an email notification to the provided email address
         send_mail(
-            f"{emergency_call.title}",
-            f"\t{emergency_call.description}\n\nThank you!",
+            f"Notification: {emergency_call.title}",
+            f"{emergency_call.description}\n\nThank you!",
             "support@helpme.co.il",
             [email_address],
             fail_silently=False,
         )
+
+        # Return a success message if the email is sent successfully
         return {"success": True, "message": "Email sent successfully."}
+
     except Exception as e:
+        # Handle exceptions that may occur during email sending
+        # Return an error message with details if an exception is raised
         return {"success": False, "error": str(e)}
