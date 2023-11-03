4349b260361af4679f0b0ae37debf4fb0a810660
amitai salmon <amitai1997@gmail.com>
2023-11-03 14:36:25 +0200

    fix urls.py

--- helpme/emergency/urls.py
+++ helpme/emergency/urls.py
@@ -20,25 +20,26 @@ from helpme.emergency.views import (
 
 app_name = "emergency"
 
+# Define the API router
 router = routers.DefaultRouter()
 router.register(r"emergency-calls", EmergencyCallViewSet)
 router.register(r"volunteers", VolunteerViewSet)
-router.register(r"rescue-teams", RescueTeamViewSet)  # optional
+router.register(r"rescue-teams", RescueTeamViewSet)  # Optional
 router.register(r"notifications", NotificationViewSet)
 router.register(r"user-locations", UserLocationViewSet)
 router.register(r"profiles", ProfileViewSet)
-router.register(r"emergency-type", EmergencyTypeViewSet)
+router.register(r"emergency-types", EmergencyTypeViewSet)
 
+# Define the URL patterns
 urlpatterns = [
+    # Regular views
     path("volunteers/map/", CustomAdminView, name="volunteers_map"),
-    path("volunteers/update/", UpdateVolunteersView, name="updated_volunteers"),
-    path("view_volunteer_location/<int:volunteer_id>/", view_volunteer_location, name="view_volunteer_location"),
+    path("volunteers/update/", UpdateVolunteersView, name="update_volunteers"),
+    path("volunteers/<int:volunteer_id>/location/", view_volunteer_location, name="volunteer_location"),
+    # Ratings and API views
     path("ratings/", include("star_ratings.urls", namespace="ratings")),
-    # URL for rating a volunteer
-    path("volunteer/<int:volunteer_id>/rate/", rate_volunteer, name="rate_volunteer"),
-    path("volunteer/<int:volunteer_id>/info/", volunteer_rating_info, name="volunteer_detail"),
-    path("", include(router.urls)),
+    path("api/volunteers/<int:volunteer_id>/rate/", rate_volunteer, name="rate_volunteer"),
+    path("api/volunteers/<int:volunteer_id>/info/", volunteer_rating_info, name="volunteer_info"),
+    # Include API routes
+    path("api/", include(router.urls)),
 ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
